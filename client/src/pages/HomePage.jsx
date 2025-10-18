33127bc0b76c069df63eae1d92ec6a87aec4436c
Shubhrati patel <shubhratipatel03@gmail.com>
2025-10-19 00:25:07 +0530

    Changed homepage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer.jsx'
-import RightSidebar from '../components/RightSideBar.jsx'
+import RightSidebar from '../components/RightSidebar.jsx'
 import { ChatContext } from '../../context/ChatContext.jsx'
 
 const HomePage = () => {
